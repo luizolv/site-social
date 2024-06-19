71e49fc0c1548b06990674fa078ae50bfa79ac3e
Gordelas <106085396+luizolv@users.noreply.github.com>
2024-06-19 13:55:46 -0300

    codigos3

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const caixaPerguntas = document.querySelector('.caixa=perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
+
+const Perguntas = [
+    {
+        enunciado: "Pergunta 1"
+        alternativas: [
+            "Altenativa 1"
+            "Altenativa 2"
+        ]
+    },
+    {
+    enunciado: "Pergunta 2"
+    alternativas: [
+        "Altenativa 1"
+        "Altenativa 2"
+        ]
+    },
+    {
+    enunciado: "Pergunta 3"
+    alternativas: [
+        "Altenativa 1"
+        "Altenativa 2"
+    ]
+    }
+]
